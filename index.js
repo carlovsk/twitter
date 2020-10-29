a9667ae7247ee11225f9b536af696961a0f164e6
Carlos Daniel <carlosdnbarboza@gmail.com>
2020-10-28 22:28:26 -0300

    starting es6 syntax

--- index.js
+++ index.js
@@ -1,74 +1,74 @@
-let Twit = require('twit');
-require('dotenv').config();
+import Twit from 'twit';
+import auth from 'dotenv';
 
+auth.config();
 
 const Bot = new Twit({
-    consumer_key:         process.env.CONSUMER_KEY,
-    consumer_secret:      process.env.CONSUMER_SECRET,
-    access_token:         process.env.ACCESS_TOKEN,
-    access_token_secret:  process.env.ACCESS_TOKEN_SECRET,
-    timeout_ms:           60 * 1000,
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+  timeout_ms: 60 * 1000,
 });
 
-var contRt = 1;
-var contAll = 1;
+let contRt = 1;
+let contAll = 1;
 
 function BotInit() {
-    let date = new Date();
-    
-    let year = date.getFullYear();
-    let month = (date.getMonth()) + 1;
-    let day = date.getDate();
+  let date = new Date();
 
-    let query = {
-        // q: `cave`,
-        // q: `cave since:${year}-${month}-${day}`,
-        q: `among us since:${year}-${month}-${day}`,
-        count: 100000,
-    }
+  let year = date.getFullYear();
+  let month = (date.getMonth()) + 1;
+  let day = date.getDate();
 
-    Bot.get('search/tweets', query, function(err, data, response) {
-        for (let i in data) {
-            if (!data.hasOwnProperty(i)) continue;
+  let query = {
+    q: `among us since:${year}-${month}-${day}`,
+    count: 100000,
+  }
 
-            let obj = data[i];
+  Bot.get('search/tweets', query, function (err, data, response) {
+    for (let i in data) {
+      if (!data.hasOwnProperty(i)) continue;
 
-            for (let prop in obj) {
-                if (!obj.hasOwnProperty(prop)) continue;
+      let obj = data[i];
 
-                if (err) {
-                    console.log(`Bot could not find latest tweet, : ` + err);
-                } else {
-                    var id = {
-                        id : data.statuses[0].id_str
-                    }
-                    
-                    Bot.post(`statuses/retweet/:id`, id, BotRetweeted);
-        
-                    function BotRetweeted(error, response) {
-                            if (error) {
-                            // console.log(`Bot could not retweet, : ` + error);
-                        } else {
-                            let curHour = date.getHours();
-                            let curMinute = date.getMinutes();
-                            let curSeconds = date.getSeconds();
+      for (let prop in obj) {
+        if (!obj.hasOwnProperty(prop)) continue;
 
-                            console.log(`Bot retweeted: ${id.id} at ${curHour}:${curMinute}:${curSeconds}.`);
-                            console.log(`Número de RTs: ${contRt} \n`);
-                            contRt++;
-                        }
-                    } 
-                }
+        if (err) {
+          console.log('\x1b[32m', `Bot could not find latest tweet, : ` + err);
+        } else {
+          var id = {
+            id: data.statuses[0].id_str
+          }
+
+          Bot.post(`statuses/retweet/:id`, id, BotRetweeted);
+
+          function BotRetweeted(error, response) {
+            if (error) {
+              // console.log(`Bot could not retweet, : ` + error);
+            } else {
+              let curHour = date.getHours();
+              let curMinute = date.getMinutes();
+              let curSeconds = date.getSeconds();
+
+              console.log('\x1b[32m', `Bot retweeted: ${id.id} at ${curHour}:${curMinute}:${curSeconds}.`);
+              console.log('\x1b[32m', `Número de RTs: ${contRt} \n`);
+              contRt++;
             }
+          }
         }
-    });
+      }
+    }
+  });
 
-    console.log(`Contagem: ${contAll}`);
-    contAll++;
+  console.log('\x1b[32m', `Contagem: ${contAll}`);
+  contAll++;
+
+  setTimeout(BotInit, 10000);
 }
 
 console.clear();
-console.log('Bot ligado!');
+console.log('\x1b[32m', 'Bot ligado!');
 
 BotInit();
-setInterval(BotInit, 10000);
--- server.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-
-app.listen(process.env.PORT || 4000);
